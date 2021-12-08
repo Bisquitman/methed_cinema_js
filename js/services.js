89ec4edfb980c4b84c569f231e8fbe1008dbe22b
Bisquitman <lynx8309@gmail.com>
2021-12-08 13:41:49 +0300

    Получение данных с сервера

---
+++ js/services.js
@@ -0,0 +1,23 @@
+const API_KEY = 'a6e08960e811613bb75e8cad7a1dea15';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const LANG = '&language=ru-RU';
+
+// REST_URL = trending/all/day?api_key=<<api_key>> 
+
+const getData = (url) => {
+  return fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw `Что-то пошло не так, ошибка ${response.status}`
+    })
+    // .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+};
+
+
+export const getTrends = async (type = 'all', period = 'day', page = 1) => {
+  const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANG}&page=${page}`;
+  return await getData(url);
+};
